79c411190078d9c7ffd9cf99b2e50831834ed483
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-06 11:56:32 +0530

    commit

---
+++ arthimetic.java
@@ -0,0 +1,14 @@
+public class arthimetic{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int x=a+b;
+        int y=a-b;
+        int z=a/b;
+        System.out.println("Sum of two numbers:"+x);
+        System.out.println("sub of two numbers:"+y);
+        System.out.println("div of two numbers:"+z);
+    }
+
+    }
+
