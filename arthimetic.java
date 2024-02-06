2e70cfa34f5952fca5b12e9e4bc7f0c3f13604ad
Dhanyajeyakumarjk <dhanyadhanyajk@gmail>
2024-02-06 12:17:27 +0530

    firstchange

--- arthimetic.java
+++ arthimetic.java
@@ -2,13 +2,10 @@ public class arthimetic{
     public static void main(String[] args){
         int a=10;
         int b=20;
-        int x=a+b;
-        int z1=a*b;
-        int y=a-b;
-        int z=a/b;
+        int x=a*b;
+
         System.out.println("Sum of two numbers:"+x);
-        System.out.println("sub of two numbers:"+y);
-        System.out.println("div of two numbers:"+z);
+        
     }
 
     }
